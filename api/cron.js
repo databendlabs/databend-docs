79a409801bbaf968ee0e9efcd419c662e437b761
q <787025321@qq.com>
2023-11-06 19:21:02 +0800

    ci: cron

--- api/cron.js
+++ api/cron.js
@@ -6,14 +6,13 @@ export default async function handler() {
   const VERCEL_TEAM_ID = process.env.VERCEL_TEAM_ID;
   const deployUrl = 'https://api.vercel.com/v12/now/deployments';
   await axios.post(deployUrl, {
+    name: 'databend-docs',
+    project: VERCEL_PROJECT_ID,
+    ...(VERCEL_TEAM_ID ? { teamId: VERCEL_TEAM_ID } : {})
+  }, {
     headers: {
       'Authorization': `Bearer ${VERCEL_TOKEN}`,
       'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: 'databend-docs',
-      project: VERCEL_PROJECT_ID,
-      ...(VERCEL_TEAM_ID ? { teamId: VERCEL_TEAM_ID } : {})
-    })
+    }
   });
 }
