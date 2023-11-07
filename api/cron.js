8544064c01beca9c537ae9fb374b17cd256f17d8
Quan <787025321@qq.com>
2023-11-07 21:43:54 +0800

    fix: auto deploy (#7)
    * fix: auto deploy
    
    * fix: auto deploy

--- api/cron.js
+++ api/cron.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
-export default async function handler() {
-  await axios.post(process.env.VERCEL_AUTO_DEPLOY);
+export default async function handler(req, res) {
+  try {
+    const response = await axios.post(process.env.VERCEL_AUTO_DEPLOY);
+    res.status(200).json({ success: true, data: response.data });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
 }
