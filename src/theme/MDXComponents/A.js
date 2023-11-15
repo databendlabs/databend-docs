1f6d6696462a2e65152575fe1d9e996691345847
Quan <787025321@qq.com>
2023-11-16 00:33:12 +0800

    chore: rc-ttoltip (#45)
    * chore: try cloud media
    
    * chore: replaced rc-tooltip

--- src/theme/MDXComponents/A.js
+++ src/theme/MDXComponents/A.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Link from '@docusaurus/Link';
 import LinkSvg from '@site/src/icons/link';
 import styles from './styles.module.scss';
-import { Tooltip } from 'antd';
+import Tooltip from 'rc-tooltip';
 function hasProtocolPrefix(str) {
   return str.startsWith('http://') || str.startsWith('https://');
 }
 export default function MDXA(props) {
   if (hasProtocolPrefix(props?.href)) {
-    return <Tooltip destroyTooltipOnHide overlayClassName={styles.tooltip} title="Open in the new tab">
+    return <Tooltip
+      placement='top'
+      destroyTooltipOnHide
+      overlayClassName={styles.tooltip} 
+      showArrow={false}
+      trigger={['hover']} 
+      overlay="Open in the new tab">
     <span className={styles.linkWrap}>
       <a {...props} target="_blank" rel="noopener noreferrer" />
       <LinkSvg/>
