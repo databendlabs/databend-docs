c5e2ccbdea1bf8c9706a21260071f3c1abe96d59
q <787025321@qq.com>
2023-11-21 19:30:57 +0800

    fix: cloud links

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,13 +1,11 @@
 import axios from 'axios';
 import docusaurusConfig from '@generated/docusaurus.config';
-import { isChinaArea } from '../utils/tools';
-const { customFields: { askBendUrl } = {} } = docusaurusConfig;
-export const CLOUD_LINK = isChinaArea() ? 'https://app.databend.cn' :'https://app.databend.com';
+const { customFields: { askBendUrl, cloudLink } = {} } = docusaurusConfig;
 export function getAnswers(question: string) {
   return axios.post(`${askBendUrl}/query`, {
     query: question
   });
 }
 export function evaluateDocs(data: {domain: string, path: string, action: 'Yes' | 'No', comment?: string}) {
-  return axios.post(`${CLOUD_LINK}/api/v1/doc-feedbacks`, data);
+  return axios.post(`${cloudLink}/api/v1/doc-feedbacks`, data);
 }
