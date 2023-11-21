ee7c08a1abbd8b65aa1fb958990cc2edb04d2083
q <787025321@qq.com>
2023-11-21 15:10:04 +0800

    feat: add feedback

--- src/theme/DocPaginator/index.js
+++ src/theme/DocPaginator/index.js
@@ -1,14 +1,80 @@
-import React from 'react';
-import Translate, {translate} from '@docusaurus/Translate';
-import PaginatorNavLink from '@theme/PaginatorNavLink';
+import React, { useState } from 'react';
+import {translate} from '@docusaurus/Translate';
+// import PaginatorNavLink from '@theme/PaginatorNavLink';
+import $t from '@site/src/utils/tools';
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 import JoinCommunity from '@site/src/components/JoinCommunity';
 import styles from './styles.module.scss';
 import Link from '@docusaurus/Link';
+import { LoadingOutlined } from '@ant-design/icons';
+import { Popover, Button, Spin, Input} from 'antd';
 import { ArrowLeft, ArrowRight } from '@site/src/components/Icons';
+import { evaluateDocs } from '@site/src/api';
+import ThumbsUp from './ThumbsUp.svg';
+import ThumbsDown from './ThumbsDown.svg';
+const { TextArea } = Input;
 export default function DocPaginator(props) {
   const {previous, next} = props;
+  const [commentValue, setCommentValue] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [isVoted, setIsVoted] = useState(false);
+  function popover() {
+    return (
+      <div style={{width: '390px', textAlign: 'center'}}>
+        <div style={{marginBottom: '8px', textAlign: 'left'}}>{$t('Leave an optional comment…')}</div>
+        <TextArea value={commentValue} onChange={(e)=> setCommentValue(e?.target?.value)} rows={5}></TextArea>
+        <Button 
+        loading={loading}
+        onClick={()=> submitInfo('No')} style={{marginTop: '16px', fontWeight: 700}} type='primary'>{$t('Vote')}</Button>
+      </div>
+    )
+  }
+  async function submitInfo(type) {
+    if (ExecutionEnvironment.canUseDOM) {
+      try {
+        setLoading(true);
+        const res = await evaluateDocs({
+          domain: window?.location?.origin,
+          path: window?.location?.pathname,
+          action: type,
+          comment: commentValue
+        })
+        console.log(res, 'res')
+        setIsVoted(true);
+        setCommentValue('')
+      } finally {
+        setLoading(false);
+      }
+    }
+  }
   return (
     <div>
+      <Spin spinning={loading} indicator={<LoadingOutlined style={{ fontSize: 24 }} />}>
+        <div className={styles.evaluation}>
+          {
+            isVoted
+            ? <span>{$t('Thanks for voting!')}</span>
+            : <>
+                <span>{$t('Did this page help you?')}</span>
+                <div 
+                  onClick={()=> submitInfo('Yes')}
+                  className={styles.button}>
+                  <ThumbsUp></ThumbsUp> Yes
+                </div>
+                <Popover
+                showArrow={false} 
+                trigger={['click']}
+                content={
+                  popover()
+                }>
+                  <div className={styles.button}>
+                    <ThumbsDown></ThumbsDown> No
+                  </div>
+                </Popover>
+              </>
+          }
+        </div>
+      </Spin>
        <nav
         className="pagination-nav docusaurus-mt-lg"
         aria-label={translate({
