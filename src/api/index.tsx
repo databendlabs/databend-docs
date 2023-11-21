46cc897a4d8ed59e35d35e5955561d828dff5b94
q <787025321@qq.com>
2023-11-21 19:21:53 +0800

    fix: api

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import docusaurusConfig from '@generated/docusaurus.config';
+import { isChinaArea } from '../utils/tools';
 const { customFields: { askBendUrl } = {} } = docusaurusConfig;
+export const CLOUD_LINK = isChinaArea() ? 'https://app.databend.cn' :'https://app.databend.com';
 export function getAnswers(question: string) {
   return axios.post(`${askBendUrl}/query`, {
     query: question
   });
 }
 export function evaluateDocs(data: {domain: string, path: string, action: 'Yes' | 'No', comment?: string}) {
-  return axios.post('/api/v1/doc-feedbacks', data);
+  return axios.post(`${CLOUD_LINK}/api/v1/doc-feedbacks`, data);
 }
