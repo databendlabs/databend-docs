9a4425ba81469e156c45fcbf058cbbb42d5356fe
soyeric128 <soyeric128@yahoo.com>
2023-12-11 23:27:23 -0500

    updates

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Translate, {translate} from '@docusaurus/Translate';
-import {PageMetadata} from '@docusaurus/theme-common';
+import Translate, { translate } from '@docusaurus/Translate';
+import { PageMetadata } from '@docusaurus/theme-common';
 import Layout from '@theme/Layout';
 import { useMount } from 'ahooks';
 import NotFoundSvg from './NotFound.svg';
@@ -11,15 +11,15 @@ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 export default function NotFound() {
-  const {siteConfig: {customFields: {homeLink}}} = useDocusaurusContext();
+  const { siteConfig: { customFields: { homeLink } } } = useDocusaurusContext();
   const pathname = ExecutionEnvironment.canUseDOM ? window?.location?.href : '';
   const redirectsMap = {
     '/doc/sql-reference': '/sql/sql-reference',
     '/doc/sql-commands': '/sql/sql-commands',
     '/doc/sql-functions': '/sql/sql-functions',
-    "/doc/develop": '/developer'
+    "/doc/develop": '/developer/drivers'
   };
-  useMount(()=> {
+  useMount(() => {
     for (const [oldPath, newPath] of Object.entries(redirectsMap)) {
       if (pathname?.includes(oldPath)) {
         window.open(pathname.replace(oldPath, newPath), '_self');
@@ -38,19 +38,19 @@ export default function NotFound() {
         <main className="container margin-vert--xl">
           <div className="row">
             <div className={clsx("col col--6 col--offset-3", styles.notFoundWrap)}>
-             <NotFoundSvg />
+              <NotFoundSvg />
               <p className={styles.title}>
                 <Translate
                   id="theme.NotFound.p1"
                   description="The first paragraph of the 404 page">
                   We could not find what you were looking for.
                 </Translate>
-                { 
-                Object.keys(redirectsMap).find(key => pathname.includes(key)) &&
+                {
+                  Object.keys(redirectsMap).find(key => pathname.includes(key)) &&
                   <span> Redirecting to a new link <Spin></Spin></span>
                 }
               </p>
-              <p style={{marginBottom: '1rem'}}>
+              <p style={{ marginBottom: '1rem' }}>
                 <Translate
                   id="theme.NotFound.p2"
                   description="The 2nd paragraph of the 404 page">
@@ -58,10 +58,10 @@ export default function NotFound() {
                   original URL and let them know their link is broken.
                 </Translate>
               </p>
-             <Space size={16}>
-              <Button type='primary' href='/doc'>Docs</Button>
-              <Button href={homeLink+"/blog"} target='_blank'>Blog</Button>
-             </Space>
+              <Space size={16}>
+                <Button type='primary' href='/doc'>Docs</Button>
+                <Button href={homeLink + "/blog"} target='_blank'>Blog</Button>
+              </Space>
             </div>
           </div>
         </main>
