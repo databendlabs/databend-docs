1cb6f3877032957cb5f8e14d265314c4507a2b27
Quan <787025321@qq.com>
2024-01-25 18:08:40 +0800

    feat: Add a progress bar at the top (#446)

--- src/theme/DocPage/index.js
+++ src/theme/DocPage/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import DocPage from '@theme-original/DocPage';
 import styles from './styles.module.scss';
+import ProgressBar from "react-scroll-progress-bar";
 
 export default function DocPageWrapper(props) {
   return (
     <>
+      <div className={styles.ProgressBar}><ProgressBar height="2px" bgcolor="var(--ifm-color-primary)" duration="0.2"/></div>
       <div className={styles.fakeBg}></div>
       <div className={styles.cell}></div>
       <DocPage {...props} />
