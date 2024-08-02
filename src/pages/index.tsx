f0291c4d27144135389e7c5809108c9ec70cd111
q <787025321@qq.com>
2024-08-02 20:19:34 +0800

    back

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+
+const Home = () => {
+  useEffect(() => {
+    if (ExecutionEnvironment.canUseDOM) {
+      window.location.href = "/guides";
+    }
+  }, []);
+
+  return <></>;
+};
+
+export default Home;
