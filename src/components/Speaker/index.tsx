52bb0e28ccf53df7b80450a83fadb88edb536870
Quan <787025321@qq.com>
2024-08-09 00:23:47 +0800

    fix: mp3 (#1128)

--- src/components/Speaker/index.tsx
+++ src/components/Speaker/index.tsx
@@ -1,19 +1,32 @@
 // Copyright 2024 DatabendLabs.
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, useRef } from "react";
 import SpeakerSvg from "@site/static/icons/speaker.svg";
 import styles from "./style.module.scss";
+import AudioDatabend from "@site/static/public/databend.mp3";
+import { useMount } from "ahooks";
 
 const Speaker: FC = (): ReactElement => {
+  const ref = useRef<HTMLAudioElement>(undefined);
+  useMount(() => {
+    ref.current.pause();
+  });
   return (
     <span
       className={styles.out}
       onClick={() => {
-        var utterance = new SpeechSynthesisUtterance("Databend");
-        utterance.lang = "en-US";
-        speechSynthesis.speak(utterance);
+        // var utterance = new SpeechSynthesisUtterance("Databend");
+        // utterance.lang = "en-US";
+        // speechSynthesis.speak(utterance);
+        ref.current.play();
       }}
     >
-      <SpeakerSvg></SpeakerSvg>
+      <SpeakerSvg />
+      <audio
+        loop={false}
+        style={{ display: "none" }}
+        ref={ref}
+        src={AudioDatabend}
+      />
     </span>
   );
 };
