8e08967df2692b796bfff972c5eabe7c8bebcca2
Quan <787025321@qq.com>
2024-11-18 11:19:06 +0800

    fix: LanguageDocs

--- src/components/LanguageDocs/index.tsx
+++ src/components/LanguageDocs/index.tsx
@@ -1,16 +1,23 @@
 // Copyright 2023 DatabendLabs.
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import React, { FC, ReactElement, ReactNode } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 interface IProps {
-  cn: ReactNode;
-  en: ReactNode;
+  cn?: string;
+  en?: string;
 }
-const LanguageDocs: FC<IProps> = ({ cn, en }): ReactElement => {
+const LanguageDocs: FC<IProps> = ({ cn = "", en = "" }): any => {
   const {
     siteConfig: {
       customFields: { isChina },
     },
   } = useDocusaurusContext() as any;
-  return <>{isChina ? cn : en}</>;
+
+  return (
+    <ReactMarkdown remarkPlugins={[remarkGfm]}>
+      {isChina ? cn : en}
+    </ReactMarkdown>
+  );
 };
 export default LanguageDocs;
