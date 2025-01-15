76e1dec72fa9ec980998ea70ae5fe6e53a4847e9
sundyli <543950155@qq.com>
2025-01-15 22:30:40 +0800

    feat: add gurubase plugin

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -115,6 +115,7 @@ const config: Config = {
     "docusaurus-plugin-sass",
     "./src/plugins/global-sass-var-inject",
     "./src/plugins/fetch-databend-releases",
+    require.resolve("./src/plugins/gurubase-widget.js"),
     [
       "@docusaurus/plugin-content-docs",
       /** @type {import('@docusaurus/plugin-content-docs').Options} */
