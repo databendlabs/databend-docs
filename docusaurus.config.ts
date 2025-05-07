37eebc0979b018c97639156d835f4251ab990f30
z <787025321@qq.com>
2025-05-07 17:26:48 +0800

    fix: Redirect iceberg (#1986)
    * fix: redirect
    
    * fix: redirect

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -209,8 +209,18 @@ const config: Config = {
           {
             from: '/',
             to: '/guides/'
+          },
+          {
+            from: '/sql/sql-reference/table-engines/iceberg',
+            to: '/guides/access-data-lake/iceberg/'
           }
-        ]
+        ],
+        createRedirects(existingPath) {
+          if (existingPath.startsWith('/guides/community/rfcs/')) {
+            return existingPath.replace('/guides/community/rfcs/', '/developer/community/rfcs/');
+          }
+          return undefined;
+        }
       }
 
     ]
