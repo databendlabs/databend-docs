9745db99542a3e4321906cd6609016632a13c015
q <787025321@qq.com>
2025-05-14 09:38:51 +0800

    feat: replaced Babel to SWC

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,11 +38,15 @@ const config: Config = {
     "Databend - Your best alternative to Snowflake. Cost-effective and simple for massive-scale analytics.",
   url: docsHomeLink,
   baseUrl: "/",
+  onBrokenAnchors: "ignore",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
   favicon: "img/rect-icon.png",
   organizationName: "DatabendLabs",
   projectName: 'Databend', // Usually your repo name.
+  future: {
+    experimental_faster: true,
+  },
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
