bf16a873fef6d2c1d02410f3b60c21c53492c120
q <787025321@qq.com>
2025-07-31 14:19:05 +0800

    feat: add reo.dev tracking

---
+++ static/ReoDev/index.js
@@ -0,0 +1,12 @@
+!(function () {
+  var e, t, n;
+  (e = '3af57862780586e'),
+    (t = function () {
+      Reo.init({ clientID: '3af57862780586e' });
+    }),
+    ((n = document.createElement('script')).src =
+      'https://static.reo.dev/' + e + '/reo.js'),
+    (n.defer = !0),
+    (n.onload = t),
+    document.head.appendChild(n);
+})();
