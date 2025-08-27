e2da61d33730c960783c42fbcf30b83de39d6399
q <787025321@qq.com>
2025-08-27 17:32:25 +0800

    feat: add Raise ticket action (#2723)

--- src/theme/DocPaginator/index.tsx
+++ src/theme/DocPaginator/index.tsx
@@ -14,7 +14,20 @@ import ThumbsUp from "./ThumbsUp.svg";
 import ThumbsDown from "./ThumbsDown.svg";
 import clsx from "clsx";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import { useLocation } from "@docusaurus/router";
 const { TextArea } = Input;
+function Alarm() {
+  return (
+    <svg
+      style={{ width: "18px" }}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      fill="currentColor"
+    >
+      <path d="M506.3 417l-213.3-364C284.8 39 270.4 32 256 32C241.6 32 227.2 39 218.1 53l-213.2 364C-10.59 444.9 9.851 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM52.58 432L255.1 84.8L459.4 432H52.58zM256 337.1c-17.36 0-31.44 14.08-31.44 31.44c0 17.36 14.11 31.44 31.48 31.44s31.4-14.08 31.4-31.44C287.4 351.2 273.4 337.1 256 337.1zM232 184v96C232 293.3 242.8 304 256 304s24-10.75 24-24v-96C280 170.8 269.3 160 256 160S232 170.8 232 184z"></path>
+    </svg>
+  );
+}
 export default function DocPaginator(props) {
   const { previous, next } = props;
   const [loading, setLoading] = useState(false);
@@ -22,9 +35,10 @@ export default function DocPaginator(props) {
   const [form] = Form.useForm();
   const {
     siteConfig: {
-      customFields: { isChina },
+      customFields: { isChina, site },
     },
   } = useDocusaurusContext() as any;
+  const { pathname } = useLocation();
   function popover() {
     return (
       <div style={{ width: "390px", textAlign: "center" }}>
@@ -138,6 +152,15 @@ export default function DocPaginator(props) {
               </div>
             </div>
           )}
+          <Button
+            target="_blank"
+            rel="noopener noreferrer"
+            href={`https://github.com/databendlabs/databend-docs/issues/new?title=Issue on docs, site=${site}&body=Path: ${pathname}`}
+            className={styles.RaiseButton}
+          >
+            <Alarm />
+            {$t("Raise issue")}
+          </Button>
         </div>
       </Spin>
       <nav
