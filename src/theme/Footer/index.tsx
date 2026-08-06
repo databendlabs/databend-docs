61ec69f254041fd2cb7c239738bf7d6ee53175a5
q <787025321@qq.com>
2026-08-06 14:35:33 +0800

    feat: add rb2b

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -151,6 +151,11 @@ function Footer() {
           }
           `}
           </script>
+          {!isChina && (
+            <script>
+              {`<script>!function(key) {if (window.reb2b) return;window.reb2b = {loaded: true};var s = document.createElement("script");s.async = true;s.src = "https://ddwl4m2hdecbv.cloudfront.net/b/" + key + "/" + key + ".js.gz";document.getElementsByTagName("script")[0].parentNode.insertBefore(s, document.getElementsByTagName("script")[0]);}("4O7Z0HZJJ8NX");</script>`}
+            </script>
+          )}
         </Head>
       )}
       <div className={clsx("footer-items", styles.footerItems)}>
