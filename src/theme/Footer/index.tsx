394357d08022cbaaae62da8feebbd2660f529b5b
q <787025321@qq.com>
2026-08-07 10:15:55 +0800

    fix: re2b

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -152,9 +152,7 @@ function Footer() {
           `}
           </script>
           {!isChina && (
-            <script>
-              {`<script>!function(key) {if (window.reb2b) return;window.reb2b = {loaded: true};var s = document.createElement("script");s.async = true;s.src = "https://ddwl4m2hdecbv.cloudfront.net/b/" + key + "/" + key + ".js.gz";document.getElementsByTagName("script")[0].parentNode.insertBefore(s, document.getElementsByTagName("script")[0]);}("W6Z57HDQ7LOX");</script>`}
-            </script>
+            <script>{`!function(key) {if (window.reb2b) return;window.reb2b = {loaded: true};var s = document.createElement("script");s.async = true;s.src = "https://ddwl4m2hdecbv.cloudfront.net/b/" + key + "/" + key + ".js.gz";document.getElementsByTagName("script")[0].parentNode.insertBefore(s, document.getElementsByTagName("script")[0]);}("W6Z57HDQ7LOX");`}</script>
           )}
         </Head>
       )}
